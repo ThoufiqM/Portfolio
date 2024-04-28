04a0d9ef75b84e56e73a8a5ede9190c5a30e0692
Ashwin <ashwin.angadi1@gmail.com>
2024-04-28 16:26:19 +0530

    resume link

--- components/Cards/ResumeButton.jsx
+++ components/Cards/ResumeButton.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 const ResumeButton = () => {
-  const handleDownload = () => {
-    // Replace 'example.pdf' with the actual path to your PDF file
-    const pdfUrl = "../../public/mtresume.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.setAttribute("download", "Mohammad_Thoufiq_Resume.pdf");
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+
+  const link = "https://drive.google.com/file/d/10_RlJwcq53rAsfzmQrZxRfzWDUGn8KQI/view?usp=drive_link";
+  
   return (
-    <div
-      onClick={handleDownload}
-      className="bg-gradient-to-r from-[#F9DB43] rounded-full p-[1px] min-w-[150px] w-full max-w-[220px] to-[#FD495E]"
-    >
-      <Button variant="secondary" className="rounded-full bg-[#161513]">
-        <span className="  bg-clip-text text-sm md:text-base font-semibold text-transparent bg-gradient-to-r from-[#F9DB43] to-[#FD495E]">
-          Resume
-        </span>
-      </Button>
+    <div className="bg-gradient-to-r from-[#F9DB43] rounded-full p-[1px] min-w-[150px] w-full max-w-[220px] to-[#FD495E]">
+      <Link href={link} target="_blank">
+        <Button variant="secondary" className="rounded-full bg-[#161513]">
+          <span className="  bg-clip-text text-sm md:text-base font-semibold text-transparent bg-gradient-to-r from-[#F9DB43] to-[#FD495E]">
+            Resume
+          </span>
+        </Button>
+      </Link>
     </div>
   );
 };
